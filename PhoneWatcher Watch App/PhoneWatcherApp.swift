cee68bfc81141f9255e5b867bc83770df7105ea3
Calvin <calvin@edu.ink>
2023-03-28 17:13:18 -0700

    Initial Commit

---
+++ PhoneWatcher Watch App/PhoneWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneWatcherApp.swift
+//  PhoneWatcher Watch App
+//
+//  Created by Calvin on 2023-03-28.
+//
+
+import SwiftUI
+
+@main
+struct PhoneWatcher_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhoneWatcher/PhoneWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneWatcherApp.swift
+//  PhoneWatcher
+//
+//  Created by Calvin on 2023-03-28.
+//
+
+import SwiftUI
+
+@main
+struct PhoneWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
